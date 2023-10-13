ba5275e6e659a1488277f721f0d600873ae4139c
Poltergeis <julio.cruzazul@hotmail.com>
2023-10-13 15:40:46 -0600

    by JULIO: objeto citas finalizado

--- rutas/citas.js
+++ rutas/citas.js
@@ -11,3 +11,53 @@ const citasSchema = new mongoose.Schema({
     "tituloCita": String,
     "descripcion": String
   });
+
+  const Citas = mongoose.model('Citas', citasSchema, 'Citas');
+
+  router.get('/', async (req, res) => {
+    try {
+        const citas = await Citas.find();
+        res.json(citas);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener las citas' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+      const cita = new Citas(req.body);
+      await cita.save();
+      res.status(201).json(cita);
+    } catch (error) {
+      res.status(400).json({ error: 'Error al crear la cita' });
+    }
+  });
+
+  router.put('/:citaId', async (req, res) => {
+    try {
+        const citaId = req.params.citaId;
+        const cita = req.body;
+        const resultado = await Citas.findByIdAndUpdate(citaId, cita, { new: true });
+        if (!resultado) {
+        res.status(404).json({ error: 'Cita no encontrada' });
+        } else {
+        res.json(resultado);
+        }
+    } catch (error) {
+        res.status(400).json({ error: 'Error al actualizar la cita' });
+    }
+});
+
+router.delete('/:citaId', async (req, res) => {
+    try {
+      const citaId = req.params.citaId;
+      const resultado = await Citas.findByIdAndDelete(citaId);
+      if (!resultado) {
+        res.status(404).json({ error: 'Cita no encontrada' });
+      } else {
+        res.json({ message: 'Cita eliminada exitosamente' });
+      }
+    } catch (error) {
+      res.status(400).json({ error: 'Error al eliminar la cita' });
+    }
+});
