8a36d90cecb623c31e0ef871b638ea09b9b519fb
Poltergeis <julio.cruzazul@hotmail.com>
2023-10-13 15:14:26 -0600

    by JULIO: 'termine las rutas para la coleccion de abogados, para el manejo de abogados propongo usar todos los cuatro metodos de mongo, debido a que los abogados son empleados que vendran y se iran con el paso del tiempo'

--- rutas/abogados.js
+++ rutas/abogados.js
@@ -12,3 +12,47 @@ const abogadoSchema = new mongoose.Schema({
   });
 
   const Abogado = mongoose.model('Abogado', abogadoSchema, 'Citas');
+
+  router.get('/', async (req,res) => {
+    try {
+      const abogado = new Abogado(req.body);
+      await abogado.save();
+      res.status(201).json(abogado);
+    } catch (error) {
+      res.status(400).json('error en el recibo de datos:\n' + {error: error.message});
+    }
+  });
+
+  router.post('/', async (req, res) => {
+    try {
+      const abogado = new Abogado(req.body);
+      await abogado.save();
+      res.status(201).json(abogado);
+    } catch (error) {
+      res.status(400).json('error en el recibo de datos:\n' + {error: error.message});
+    }
+  });
+
+  router.put('/:id', async (req, res) => {
+    try {
+      const abogado = await Abogado.findByIdAndUpdate(req.params.id, req.body, {new: true}); 
+      if (!abogado) {
+        return res.status(404).json({ message:'abogado no encontrado'});
+      }
+      res.status(200).json(abogado);
+    } catch (error) {
+      res.status(400).json({error: error.message});
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const abogado = await Abogado.findByIdAndRemove(req.params.id);
+      if (!abogado) {
+        return res.status(404).json({ message: 'Abogado no encontrado'});
+      }
+      res.status(204).send();
+    } catch (error) {
+      res.status(400).json({error: error.message});
+    }
+  });
